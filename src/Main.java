647cdf3bd095b499d610ea54045932dd4b3df0a7
Igor Putyajkin <putyajkin99@gmail.com>
2023-09-21 03:29:09 +0300

    11.2.1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> todoList = new ArrayList<>();
+        HashSet<String> todoList = new HashSet<>();
         todoList.add("Купить молоко");
-        todoList.add("Купить лошадь");
+        todoList.add("Купить лошадь");//возврвщает булево значение
         todoList.add("Покормить кота");
-        todoList.add("Купить лошать");
+
+        boolean horseAgain = todoList.add("Купить лошадь");
+        System.out.println(horseAgain);
         todoList.add("Покормить кота");
-        for(String item:todoList){
+
+        todoList.remove("Купить лошадь");
+
+        for (String item : todoList) {
             System.out.println(item);
         }
+
+        System.out.println(todoList.contains("sdfsadfa"));//есть ли такой объект
+        System.out.println(todoList.isEmpty());
+        System.out.println(horseAgain);
+        System.out.println(todoList.size());
+
+//        todoList.clear();
+//        for (String iter : todoList) {
+//            System.out.println(iter);
+//        }
     }
 }
