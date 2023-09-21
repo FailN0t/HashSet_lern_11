77de64a35dd0e10e0814d6a69546a58849b82558
Igor Putyajkin <putyajkin99@gmail.com>
2023-09-21 09:07:14 +0300

    11.2.2

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,8 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-        HashSet<String> todoList = new HashSet<>();
-        todoList.add("Купить молоко");
-        todoList.add("Купить лошадь");//возврвщает булево значение
-        todoList.add("Покормить кота");
-
-        boolean horseAgain = todoList.add("Купить лошадь");
-        System.out.println(horseAgain);
-        todoList.add("Покормить кота");
-
-        todoList.remove("Купить лошадь");
-
-        for (String item : todoList) {
-            System.out.println(item);
-        }
-
-        System.out.println(todoList.contains("sdfsadfa"));//есть ли такой объект
-        System.out.println(todoList.isEmpty());
-        System.out.println(horseAgain);
-        System.out.println(todoList.size());
-
-//        todoList.clear();
-//        for (String iter : todoList) {
-//            System.out.println(iter);
-//        }
+        HashSet<Task> taskHashSet = new HashSet<>();
+        taskHashSet.add(new Task("покормить лошадь"));
+        System.out.println(taskHashSet.contains(new Task("Покормить лошадь")));
     }
 }
