b9fcd64cbadbeb812997120bf9b41d102977b24f
Igor Putyajkin <putyajkin99@gmail.com>
2023-09-23 00:26:44 +0300

    11.2.23

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        HashSet<Task> taskHashSet = new HashSet<>();
-        taskHashSet.add(new Task("покормить лошадь"));
-        System.out.println(taskHashSet.contains(new Task("Покормить лошадь")));
+        TreeSet<Task> taskTreeSet = new TreeSet<>(new TaskComparator());
+
+        taskTreeSet.add(new Task("Покормить лошадь"));
+
+        taskTreeSet.add(new Task("Антону напомнить об отчете"));
+
+        taskTreeSet.add(new Task("Купить молоко"));
+
+        taskTreeSet.add(new Task("Выбросить мусор"));
+
+        System.out.println(taskTreeSet.ceiling(new Task("Покормить лоша")));
+        System.out.println(taskTreeSet.floor(new Task("Купить мол")));
+        System.out.println(taskTreeSet.contains(new Task("Купить молоко")));
+        System.out.println(taskTreeSet.first());
+        System.out.println(taskTreeSet.last());
+        System.out.println(taskTreeSet.pollLast());
+        for(Task task: taskTreeSet){
+            System.out.println(task);
+        }
+        System.out.println();
+
+        System.out.println();
     }
 }
